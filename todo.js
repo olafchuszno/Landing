1d340857495eec0e10c171bbe0632e4c8c4ce699
Olaf <olaf.chuszno@gmail.com>
2024-01-07 22:54:24 +0300

    Block empty tasks and guide the user

--- todo.js
+++ todo.js
@@ -2,26 +2,35 @@ const myInput = document.getElementById("myInput");
 const submitButton = document.getElementById("submitButton");
 const taskList = document.getElementById("taskList");
 
-const liClasses = "px-2 py-1 text-center text-xl bg-myLace rounded-2xl mt-4";
+const liClasses =
+  "px-2 py-1 text-center text-xl bg-myLace rounded-2xl mt-4 hover:bg-amber-800";
 
 let taskCounter = 0;
+let failedAttempts = 0;
 
 myInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     console.log("Enter");
 
-    // Increment task counter
-    taskCounter++;
+    // Add a new task if there is a content content
+    if (myInput.value !== "") {
+      // Increment task counter
+      taskCounter++;
 
-    // Add a new task
-    addLi(myInput.value, taskCounter);
+      addLi(myInput.value, taskCounter);
+    } else {
+      if (failedAttempts > 2) {
+        alert("A task must contain something... Right?");
+      }
+      failedAttempts++;
+    }
 
     myInput.value = "";
 
-    // If it's the first task, add padding to the list
-    if (taskCounter === 1) {
-      taskList.classList.toggle("p-4");
-    }
+    // // If it's the first task, add padding to the list
+    // if (taskCounter === 1) {
+    //   taskList.classList.toggle("p-4");
+    // }
   }
 });
 
