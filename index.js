a8a948dd8e6192c94057d14df3168b59a08ec270
Olaf <olaf.chuszno@gmail.com>
2024-01-08 10:22:39 +0300

    Update

---
+++ index.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+
+body.classList.toggle("bg-black");
+
+body.classList.toggle("bg-white");
--- todo.js
+++ todo.js
@@ -1,42 +1,25 @@
 const myInput = document.getElementById("myInput");
-const submitButton = document.getElementById("submitButton");
+const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 const activeTasksDisplay = document.getElementById("activeTasks");
+const clearButton = document.getElementById("clearButton");
 
 const liClasses =
-  "px-2 py-2 text-center text-xl bg-myLace rounded-2xl mt-4 hover:bg-green-500 task";
+  "px-4 py-2 text-center text-xl bg-myLace rounded-2xl mt-4 hover:bg-green-500 task";
 
 let taskCounter = 0;
 let failedAttempts = 0;
 
+//
 myInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    console.log("Enter");
-
-    // Add a new task if there is a content content
-    if (myInput.value !== "") {
-      // Increment task counter
-      incrementTaskCounter();
-
-      addLi(myInput.value, taskCounter);
-    } else {
-      if (failedAttempts > 2) {
-        alert("A task must contain something... Right?");
-      }
-      failedAttempts++;
-    }
-
-    myInput.value = "";
-
-    // // If it's the first task, add padding to the list
-    // if (taskCounter === 1) {
-    //   taskList.classList.toggle("p-4");
-    // }
+    handleNewTask();
   }
 });
 
-submitButton.addEventListener("click", () => {
-  console.log("submited");
+// Listen for the add task button click to add the task
+addTaskButton.addEventListener("click", () => {
+  handleNewTask();
 });
 
 // Listen for task clicks
@@ -45,6 +28,11 @@ taskList.addEventListener("click", (e) => {
     e.target.remove();
     decrementTaskCounter();
   }
+});
+
+clearButton.addEventListener("click", () => {
+  taskList.innerHTML = "";
+  resetTaskCounter();
 });
 
 function addLi(content, taskCounter) {
@@ -59,6 +47,30 @@ function addLi(content, taskCounter) {
   taskList.appendChild(newLi);
 }
 
+function handleNewTask() {
+  // Add a new task if there is a content content
+  if (myInput.value !== "") {
+    // Increment task counter
+    incrementTaskCounter();
+
+    // Create the element and add it to the DOM with appropriate classes
+    addLi(myInput.value, taskCounter);
+  } else {
+    // If the user has done it more than one time display the proper message
+    if (failedAttempts > 1) {
+      alert("The task you are creating cannot be empty.");
+    } else {
+      // Guide the user
+      alert("A task must contain something... Right?");
+    }
+    // Check for more empty clicks and guide the user
+    failedAttempts++;
+  }
+
+  // Clear task input field
+  myInput.value = "";
+}
+
 function incrementTaskCounter() {
   taskCounter++;
   activeTasksDisplay.innerHTML = taskCounter;
@@ -66,5 +78,10 @@ function incrementTaskCounter() {
 
 function decrementTaskCounter() {
   taskCounter--;
+  activeTasksDisplay.innerHTML = taskCounter;
+}
+
+function resetTaskCounter() {
+  taskCounter = 0;
   activeTasksDisplay.innerHTML = taskCounter;
 }
