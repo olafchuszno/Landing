542661be48ea4d5b77affeba1483b0fce1aa084c
Olaf <olaf.chuszno@gmail.com>
2024-01-08 11:54:38 +0300

    UpdateAdd instructions functionality

--- todo.js
+++ todo.js
@@ -3,9 +3,12 @@ const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 const activeTasksDisplay = document.getElementById("activeTasks");
 const clearButton = document.getElementById("clearButton");
+const listButton = document.getElementById("listButton");
+const groupButton = document.getElementById("groupButton");
+const completedTasksCounter = document.getElementById("completedTasks");
 
 const liClasses =
-  "px-4 py-2 text-center text-xl bg-myLace rounded-2xl mt-4 hover:bg-green-500 task";
+  "px-4 py-2 text-center text-xl  bg-myLace rounded-2xl mt-4 hover:bg-green-500 task";
 
 let taskCounter = 0;
 let completedTasks = 0;
@@ -36,6 +39,35 @@ clearButton.addEventListener("click", () => {
   taskList.innerHTML = "";
   resetTaskCounter();
 });
+
+groupButton.addEventListener("click", () => {
+  // If it's green (if user was instructed) turn it off
+  if (groupButton.classList.contains("bg-green-500")) {
+    groupButton.classList.toggle("bg-green-500");
+  }
+
+  // if it's NOT active (if it's not white)
+  if (!groupButton.classList.contains("bg-white")) {
+    // Change style of the buttons
+    groupButton.classList.toggle("bg-white");
+    listButton.classList.toggle("bg-white");
+    // Allow task grouping
+    taskList.classList.toggle("flex");
+  }
+});
+
+listButton.addEventListener("click", () => {
+  // if it's NOT active
+  if (!listButton.classList.contains("bg-white")) {
+    // Change style of the buttons
+    groupButton.classList.toggle("bg-white");
+    listButton.classList.toggle("bg-white");
+    // Allow task grouping
+    taskList.classList.toggle("flex");
+  }
+});
+
+// Functions
 
 function addLi(content) {
   const newLi = document.createElement("li");
@@ -72,6 +104,10 @@ function handleNewTask() {
 function incrementTaskCounter() {
   taskCounter++;
   activeTasksDisplay.innerHTML = taskCounter;
+  if (taskCounter > 3) {
+    alert("Consider grouping your tasks. Simply click on the group button!");
+    groupButton.classList.toggle("bg-green-500");
+  }
 }
 
 function decrementTaskCounter() {
@@ -87,4 +123,9 @@ function resetTaskCounter() {
 function updateCompletedTasks() {
   completedTasks++;
   completedTasksCounter.innerHTML = completedTasks;
+  if (completedTasks % 10 === 0) {
+    alert(
+      "Every ten tasks done is ... ten things you won't have to do tomorrow!"
+    );
+  }
 }
