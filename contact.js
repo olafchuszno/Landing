d0b03c810812d979f795bf3d379e2d554a27b372
Olaf <olaf.chuszno@gmail.com>
2024-01-08 13:22:56 +0300

    Transition colors smoothly

--- contact.js
+++ contact.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const main = document.getElementById("main");
 const viewMode = document.getElementById("viewMode");
 const contactMessage = document.getElementById("contactMessage");
 
@@ -7,6 +8,13 @@ viewMode.addEventListener("click", function () {
   body.classList.toggle("bg-white");
   main.classList.toggle("text-gray-900");
   main.classList.toggle("text-myBone");
+
+  // Change button name to Light/Dark
+  if (viewMode.innerHTML === "Dark mode") {
+    viewMode.innerHTML = "Light mode";
+  } else {
+    viewMode.innerHTML = "Dark mode";
+  }
 });
 
 displayMessage();
