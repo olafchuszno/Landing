68ed61c7af6522f5ae83aed3cd6026b4fb360900
Olaf <olaf.chuszno@gmail.com>
2024-01-08 13:12:25 +0300

    Listen for view mode change on contact page

--- contact.js
+++ contact.js
@@ -1,4 +1,15 @@
+const body = document.querySelector("body");
+const viewMode = document.getElementById("viewMode");
 const contactMessage = document.getElementById("contactMessage");
+
+viewMode.addEventListener("click", function () {
+  body.classList.toggle("bg-black");
+  body.classList.toggle("bg-white");
+  main.classList.toggle("text-gray-900");
+  main.classList.toggle("text-myBone");
+});
+
+displayMessage();
 
 function displayMessage() {
   setTimeout(() => {
@@ -6,5 +17,3 @@ function displayMessage() {
     contactMessage.classList.toggle("opacity-100");
   }, 500);
 }
-
-displayMessage();
