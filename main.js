3bda169d7eb5515b3d6bb7d855f81a8ce1084a16
Olaf <olaf.chuszno@gmail.com>
2024-01-09 11:59:16 +0300

    Make active tab work on local machine

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ const viewMode = document.getElementById("viewMode");
 const main = document.getElementById("main");
 const descriptions = document.querySelectorAll("p");
 
+const mainPageLink = document.querySelector(".mainPageLink");
+const contactPageLink = document.querySelector(".contactPageLink");
+
 const url = new URL(window.location.href);
 const parsedUrl = url.pathname;
 
+// Make active tab work on local machine
+if (parsedUrl == "/index.html" || "/contact/index.html") {
+  mainPageLink.id = "/index.html";
+  contactPageLink.id = "/contact/index.html";
+}
 document.getElementById(parsedUrl).classList.toggle("bg-gray-300");
 
 viewMode.addEventListener("click", function () {
